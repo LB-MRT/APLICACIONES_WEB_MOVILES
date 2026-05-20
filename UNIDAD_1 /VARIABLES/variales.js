58b68605d71fa354d5041b707b07354249954d95
LB-MRT <lisbethbermudezz@est.uepmrmp.edu.ec>
2026-05-20 09:52:54 -0500

    Add files via upload

---
+++ UNIDAD_1 /VARIABLES/variales.js
@@ -0,0 +1,7 @@
+// const para crear una variable que no va a cambiar
+// const variable que no cambia
+let nombre="lisbeth"
+console.log(nombre)// console.log serve para IMPRIMER CON CONSOLA lisbeth 
+const val1 = 1 // const sirve para no cambiar de valor en el futuro
+console.log(val1)
+val1 = 2 
